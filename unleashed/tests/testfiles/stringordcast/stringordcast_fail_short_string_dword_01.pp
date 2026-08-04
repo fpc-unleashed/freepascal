8e7a64d398f1ed499a0350302d58a7d8c81f0a3b
fibodevy <fibodevy@gmail.com>
2026-08-04 06:14:25 +0200

    tests: move string-ord cast fold tests into suite

--- unleashed/tests/testfiles/stringordcast/stringordcast_fail_short_string_dword_01.pp
+++ unleashed/tests/testfiles/stringordcast/stringordcast_fail_short_string_dword_01.pp
@@ -4,7 +4,7 @@
 // "Cannot cast string of length 3 to ordinal type ""LongWord"" (size 4 bytes)"
 // and must not produce a cascading "Illegal expression" second error
 
-program tb0299;
+program stringordcast_fail_short_string_dword_01;
 
 {$mode unleashed}
 
